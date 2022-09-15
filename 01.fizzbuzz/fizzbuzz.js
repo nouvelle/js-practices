b4f93126045c50a87e4410a202a08fdf276e41e4
nouvelle <nouvellenuts@gmail.com>
2022-09-15 21:19:20 +0900

    feat: make fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,11 @@
+[...Array(20)].map((_, i) => i + 1).map(num => {
+  if (num % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+    console.log("Fizz");
+  } else if (num % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num);
+  }
+});
