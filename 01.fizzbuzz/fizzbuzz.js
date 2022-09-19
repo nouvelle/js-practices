2f4fcebc0f4bf4ed7d8552fc8cfbf66f0f608d75
nouvelle <nouvellenuts@gmail.com>
2022-09-20 01:27:20 +0900

    fix: より適切なメソッドを使用

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,4 +1,4 @@
-[...Array(20)].map((_, i) => i + 1).map(num => {
+[...Array(20)].map((_, i) => i + 1).forEach(num => {
   if (num % 15 === 0) {
     console.log("FizzBuzz");
   } else if (num % 3 === 0) {
